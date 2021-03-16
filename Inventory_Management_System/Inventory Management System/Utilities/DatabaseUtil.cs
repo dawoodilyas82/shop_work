2c0f722e00f5a95873bebd214d8291c8458cea33
Dawood Ilyas <52783022+dawoodilyas82@users.noreply.github.com>
2021-03-17 01:51:18 +0500

    Changed the database connection from access to MySQl

--- Inventory_Management_System/Inventory Management System/Utilities/DatabaseUtil.cs
+++ Inventory_Management_System/Inventory Management System/Utilities/DatabaseUtil.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Data.OleDb;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Inventory_Management_System.Utilities
 {
@@ -9,27 +10,22 @@ namespace Inventory_Management_System.Utilities
     {
         static string dbPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "gac.accdb");
         
-        private static string connectionString = String.Format("Provider=Microsoft.ACE.OLEDB.12.0; Data Source= {0}", dbPath);
+        //private static string connectionString = String.Format("Provider=Microsoft.ACE.OLEDB.12.0; Data Source= {0}", dbPath);
+        private static string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=gac;";
 
         public static DataTable fetchDataQuery(string query)
         {
             try
             {
-                OleDbConnection connection = new OleDbConnection(connectionString);
-                connection.Open();
-                OleDbDataAdapter adpater;
-                OleDbCommand command = new OleDbCommand();
-                command.Connection = connection;
-                command.CommandType = CommandType.Text;
-                command.CommandText = query;
-
+                MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+                MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+                commandDatabase.CommandTimeout = 60;
+                databaseConnection.Open();
                 DataTable adapter_DataTable = new DataTable();
-
-                adpater = new OleDbDataAdapter(command);
+                MySqlDataAdapter adpater = new MySqlDataAdapter(commandDatabase);
                 adpater.Fill(adapter_DataTable);
-                connection.Close();
+                databaseConnection.Close();
                 return adapter_DataTable;
-
             }
             catch (Exception ex1)
             {
@@ -38,18 +34,14 @@ namespace Inventory_Management_System.Utilities
         }
         public static bool CommandExecutionQuery(string query)
         {
-            try
-            {
-                OleDbConnection connection = new OleDbConnection(connectionString);
-                connection.Open();
-                OleDbCommand command = new OleDbCommand();
-                command.Connection = connection;
-                command.CommandType = CommandType.Text;
-                command.CommandText = query;
-                command.ExecuteNonQuery();
-                connection.Close();
+            try{
+                MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+                MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+                commandDatabase.CommandTimeout = 60;
+                databaseConnection.Open();
+                commandDatabase.ExecuteNonQuery();
+                databaseConnection.Close();
                 return true;
-
             }
             catch (Exception ex1)
             {
@@ -58,27 +50,19 @@ namespace Inventory_Management_System.Utilities
         }
         public static bool doesRecordExistAgainstQuery(string query)
         {
-            try
-            {
-                OleDbConnection connection = new OleDbConnection(connectionString);
-                connection.Open();
-                OleDbCommand command = new OleDbCommand();
-                command.Connection = connection;
-                command.CommandType = CommandType.Text;
-                command.CommandText = query;
-
-                OleDbDataReader obj = command.ExecuteReader();
-                if (obj.HasRows)
-                {
-                    connection.Close();
+            try{
+                MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+                MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+                commandDatabase.CommandTimeout = 60;
+                databaseConnection.Open();
+                MySqlDataReader reader = commandDatabase.ExecuteReader();
+                if (reader.HasRows){
+                    databaseConnection.Close();
                     return true;
                 }
-                connection.Close();
+                databaseConnection.Close();
                 return false;
-
-            }
-            catch (Exception ex1)
-            {
+            }catch (Exception ex1){
                 return false;
             }
         }
